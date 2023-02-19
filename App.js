41637776b55164d7525fb93ec1d2e71e59f528f8
salmansheri <sheriffsalman00@gmail.com>
2023-02-20 02:44:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      {text: enteredGoalText, id: Math.random().toString()},
+    ]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your course "
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.textContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goal}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index)=> {
+            return item.id; 
+          } }
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +56,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    // backgroundColor: "red",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBotton: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  textContainer: {
+    flex: 5,
+  },
+  goal: {
+    fontSize: 15,
+    margin: 8,
+    backgroundColor: "indigo",
+    color: "white",
+    borderRadius: 6,
+    padding: 8,
+  },
+  goalText: {
+    color: "white",
   },
 });
